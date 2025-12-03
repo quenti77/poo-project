b8f14f69a1a73367821af51752d9afee6df1dce1
Quentin Ysambert <quentin.ysambert@inqipit.fr>
2025-12-03 15:47:09 +0100

    feat(clean): remove all files

--- config/interfaces.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Repositories\Contracts\UsersRepositoryInterface;
-use App\Repositories\Database\DbUsersRepository;
-use Tuto\Container\Items\DependencyItem;
-
-return [
-    DependencyItem::interface(UsersRepositoryInterface::class, DbUsersRepository::class),
-];
