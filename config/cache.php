551ae668d9ad9c4197498fc9081571fe1b61cad6
Quentin Ysambert <quentin.ysambert@inqipit.fr>
2026-01-12 14:14:33 +0100

    main: remove unused env

--- config/cache.php
+++ config/cache.php
@@ -2,7 +2,6 @@
 
 use Tuto\Cache\CacheFactory;
 use Tuto\Cache\CacheInterface;
-use Tuto\Container\DependencyInjectionContainer;
 use Tuto\Container\Items\DependencyItem;
 
 return [
@@ -10,12 +9,6 @@ return [
     'cache.prefix' => env('CACHE_PREFIX', 'poo_cache'),
 
     'cache.file.path' => env('CACHE_FILE_PATH', 'storage/framework/cache'),
-
-    'cache.redis.host' => env('REDIS_HOST', '127.0.0.1'),
-    'cache.redis.port' => env('REDIS_PORT', 6379),
-    'cache.redis.password' => env('REDIS_PASSWORD'),
-    'cache.redis.database' => env('CACHE_REDIS_DATABASE', 0),
-    'cache.redis.timeout' => env('CACHE_REDIS_TIMEOUT', 2),
 
     DependencyItem::single(
         CacheInterface::class,
